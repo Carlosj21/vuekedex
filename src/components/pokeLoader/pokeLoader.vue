024ca0a1de430935e291f7f62ba6e4ef0b93dea6
Carlos Colmenares <carlosjcool@gmail.com>
2025-10-19 19:26:47 -0500

    refactor: simplify pokeLoader component structure by replacing v-img with img tag

--- src/components/pokeLoader/pokeLoader.vue
+++ src/components/pokeLoader/pokeLoader.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-container fluid class="fill-height">
-    <v-row>
-      <v-col cols="12" md="12" class="d-flex align-center justify-center">
-        <v-img class="spinning-image" height="106" width="106" src="images/pokeball.svg"></v-img>
-      </v-col>
-    </v-row>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <img class="spinning-image" src="/images/pokeball.svg" alt="pokeball" height="106" width="106">
   </v-container>
 </template>
